adce27707b97c1d590447b6a7a945b5d86cf9046
DerickIzidro <eusouderickizidro@gmail.com>
2023-10-04 11:46:48 -0300

    Update Banner and minor changes to theme

--- src/components/Banner/BannerMain.tsx
+++ src/components/Banner/BannerMain.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 // console.log(banner);
 
-export default function BannerMain(){
-    return(
-        <div>
-            <img src='/images/Banners/mainbanner.png' />
+export default function BannerMain() {
+    return (
+        <div className="bg-eeaobanner bg-no-repeat bg-cover h-2/3 w-full flex items-center justify-start md:px-24 px-0">
+            <div>
+                <h1 className='md:text-7xl text-2xl font-bold'>Rewatch +</h1>
+                <br/>
+                <div>
+                    <input placeholder="Placeholder for Searchbar" />
+                </div>
+
+            </div>
         </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
     },
     extend: {
       backgroundImage: {
+        'eeaobanner': "url('/images/Banners/mainbanner.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
