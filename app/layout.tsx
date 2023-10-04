c8ee403330c6bcbde142ac6806efcab7dfbee2ae
DerickIzidro <eusouderickizidro@gmail.com>
2023-10-04 10:58:20 -0300

    Add theme,mainbanner,colors,header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import Header from '../src/components/Header/Signin/Header';
+import BannerMain from "@/src/components/Banner/BannerMain";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -18,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className="h-full scroll-smooth">
       <body className={montserrat.className + " h-full bg-offblack"}>
-        <header className="w-full flex items-center justify-center bg-gray bg-blend-multiply backdrop-blur-sm">
+        <header className="w-full flex items-center justify-center bg-gray mix-blend-multiply backdrop-blur-sm absolute">
           <Header/>
         </header>
+        <BannerMain/>
         {children}
         <footer className="flex items-center justify-center">
           <div>Footer</div>
---
+++ src/components/Banner/BannerMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import banner from '@/public/images/Banners/mainbanner.png';
+
+// console.log(banner);
+
+export default function BannerMain(){
+    return(
+        <div>
+            <img src='/images/Banners/mainbanner.png' />
+        </div>
+    );
+}
